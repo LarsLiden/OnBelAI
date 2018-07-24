18a31912017501b6988831d37c30a7bc9fed52d4
Brooks Talley <brtalley@microsoft.com>
2018-07-24 15:07:34 -0700

    route data template

---
+++ src/data/route_template.js
@@ -0,0 +1,29 @@
+"use strict";
+{
+    "route";
+    [
+        "name", "route1",
+        "holds", {},
+        {
+            id: "start",
+            x: 123,
+            y: 456
+        },
+        {
+            id: "1",
+            x: 123,
+            y: 456
+        },
+        {
+            id: "2",
+            x: 123,
+            y: 456
+        },
+        {
+            id: "end",
+            x: 123,
+            y: 456
+        }
+    ];
+}
+//# sourceMappingURL=route_template.js.map
