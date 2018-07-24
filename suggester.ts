489652ebcf95badbcfeba894b5429a24b55a0741
Kiran Maddipati <kimaddip@microsoft.com>
2018-07-23 17:16:13 -0700

    added suggester cases around hands and hips.

--- suggester.ts
+++ suggester.ts
@@ -6,33 +6,59 @@ export class Suggester {
         const ret: string[] = [];
 
         // toes
-        // if (!leftFootMatched) {
+        if (!delta.leftFootMatched) {
             // Move up/down
             if (this.isInFirstQuadrant(delta.leftFoot) || this.isInSecondQuadrant(delta.leftFoot)) {
                 ret.push('Stand on your left toe');
             } else if (this.isInThirdQuadrant(delta.leftFoot) || this.isInFourthQuadrant(delta.leftFoot)) {
                 ret.push('Straighten your left toe')
             }
-        // }
+        }
 
-        // if (!rightFootMatched) {
+        if (!delta.rightFootMatched) {
             // Move up/down
             if (this.isInFirstQuadrant(delta.rightFoot) || this.isInSecondQuadrant(delta.rightFoot)) {
                 ret.push('Stand on your right toe');
             } else if (this.isInThirdQuadrant(delta.rightFoot) || this.isInFourthQuadrant(delta.rightFoot)) {
                 ret.push('Straighten your right toe')
             }
-        // }
+        }
 
         //hips
+        if (this.isInFirstQuadrant(delta.rightHip)) {
+            ret.push('Move hips to the right and up');
+        } else if (this.isInSecondQuadrant(delta.rightHip)) {
+            ret.push('Move hips to the left and up');
+        } else if (this.isInThirdQuadrant(delta.rightHip)) {
+            ret.push('Move hips to the left and down');
+        } else if (this.isInFourthQuadrant(delta.rightHip)) {
+            ret.push('Move hips to the right and down');
+        }
 
         // hands
-        // if (!leftHandMatched) {
+        if (!delta.leftHandMatched) {
+            if (this.isInFirstQuadrant(delta.leftHand)) {
+                ret.push('Reach right and up with your left hand');
+            } else if (this.isInSecondQuadrant(delta.leftHand)) {
+                ret.push('Reach left and up with your left hand');
+            } else if (this.isInThirdQuadrant(delta.leftHand)) {
+                ret.push('Reach left and down with your left hand');
+            } else if (this.isInFourthQuadrant(delta.leftHand)) {
+                ret.push('Reach right and down with your left hand');
+            }
+        }
 
-        // }
-        // if (!rightHandMatched) {
-
-        // }
+        if (!delta.rightHandMatched) {
+            if (this.isInFirstQuadrant(delta.rightHand)) {
+                ret.push('Reach right and up with your right hand');
+            } else if (this.isInSecondQuadrant(delta.rightHand)) {
+                ret.push('Reach left and up with your right hand');
+            } else if (this.isInThirdQuadrant(delta.rightHand)) {
+                ret.push('Reach left and down with your right hand');
+            } else if (this.isInFourthQuadrant(delta.rightHand)) {
+                ret.push('Reach right and down with your right hand');
+            }
+        }
 
         return ret;
     }
